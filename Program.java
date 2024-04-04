862f79f5a0d8ca06bf96d84bb660604b19186c4d
MelissaH <iheartumedicdroid@gmail.com>
2024-04-04 14:49:55 -0700

    feat: add unit tests

---
+++ Program.java
@@ -0,0 +1,26 @@
+/**
+ * Program
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        double outcome = add(2, 4);
+        System.out.println(outcome == 6);
+    }
+    
+    public static double add(double x, double y) {
+        return x + y;
+    }
+
+    public static boolean IsPrime(int number) {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
